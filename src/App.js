752cc374264b5db065e3c28a787a43e739cae8c5
Srujan1527 <srujanyadav95@gmail.com>
2023-01-24 14:23:52 +0530

    OTP auth/verification done

--- src/App.js
+++ src/App.js
@@ -7,45 +7,78 @@ const app = new Realms.App({ id: "application-0-pcidj" });
 
 function App() {
   const [phoneNumber, setPhoneNumber] = useState("");
+  const [isOTPSent, setIsOTPSent] = useState(false);
+  const [otp, setOtp] = useState("");
   // const [otp, setOTP] = useState("");
   // const [error, setError] = useState(null);
 
-  const handleSubmit = async (e) => {
-  
+  const loginAnonymous = async () => {
+    const credentials = Realms.Credentials.anonymous();
     try {
-      const clientFunc = await app.currentUser.callFunction(
-        "getOTP",
-        phoneNumber
-      );
-      console.log(clientFunc);
+      const user = await app.logIn(credentials);
+      console.log(user);
+      console.log("user logged in anonymously");
+    } catch (err) {
+      console.log("Failed to log in ", err);
+    }
+  };
+
+  const handleSubmit = async (e) => {
+    try {
+      if (app.currentUser === null) {
+        loginAnonymous();
+        console.log("line 29 executed");
+        await app.currentUser.callFunction("getOTP", phoneNumber);
+        console.log("Successfully Executed realm function when user is null");
+      } else {
+        await app.currentUser.callFunction("getOTP", phoneNumber);
+
+        console.log("Successfully Executed");
+        setIsOTPSent(true);
+      }
     } catch (err) {
       console.log(err);
     }
   };
 
+  const handleVerify = async () => {
+    try {
+      await app.currentUser.callFunction("handleVerify", phoneNumber, otp);
+      console.log("verification Done");
+    } catch (err) {
+      console.log(err);
+    }
+  };
   return (
-    <form>
-      {/* {error && <p>{error}</p>} */}
-      <label htmlFor="phoneNumber">Phone Number:</label>
-      <input
-        id="phoneNumber"
-        type="text"
-        value={phoneNumber}
-        onChange={(e) => setPhoneNumber(e.target.value)}
-      />
-      <button type="button" onClick={handleSubmit}>
-        Send OTP
-      </button>
-      {/* <label>
-        OTP:
-        <input
-          type="text"
-          value={otp}
-          onChange={(e) => setOTP(e.target.value)}
-        />
-      </label>
-      <button type="submit">Verify</button> */}
-    </form>
+    <>
+      {isOTPSent ? (
+        <form>
+          <label htmlFor="otp">OTP:</label>
+          <input
+            id="otp"
+            type="number"
+            value={otp}
+            onChange={(e) => setOtp(e.target.value)}
+          />
+          <button type="button" onClick={handleVerify}>
+            Verify
+          </button>
+        </form>
+      ) : (
+        <form>
+          <label htmlFor="phoneNumber">Phone Number:</label>
+          <input
+            id="phoneNumber"
+            type="text"
+            value={phoneNumber}
+            onChange={(e) => setPhoneNumber(e.target.value)}
+          />
+          <button type="button" onClick={handleSubmit}>
+            Send OTP
+          </button>
+        </form>
+      )}
+    </>
   );
 }
 
